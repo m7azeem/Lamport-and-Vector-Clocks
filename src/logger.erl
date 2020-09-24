8138c3918e8e2408d3861761496d435dd87083c5
m.azeem <m7azeem@gmail.com>
2020-09-24 11:31:51 +0400

    Add logger, worker and a test module

---
+++ src/logger.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author m.azeem
+%%%
+%%% @end
+%%% Created : 24. Sep 2020 10:59 AM
+%%%-------------------------------------------------------------------
+-module(logger).
+-author("m.azeem").
+
+%% API
+-export([start/1, stop/1]).
+
+start(Nodes) ->
+  spawn_link(fun() ->init(Nodes) end).
+
+stop(Logger) ->
+  Logger ! stop.
+
+init(_) ->
+  loop().
+
+loop() ->
+  receive
+    {log, From, Time, Msg} ->
+      log(From, Time, Msg),
+      loop();
+    stop ->
+      ok
+  end.
+
+log(From, Time, Msg) ->
+  io:format("log: ~w ~w ~p~n", [Time, From, Msg]).
+
---
+++ src/test.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author m.azeem
+%%%
+%%% @end
+%%% Created : 24. Sep 2020 11:20 AM
+%%%-------------------------------------------------------------------
+-module(test).
+-author("m.azeem").
+
+%% API
+-export([run/2]).
+
+run(Sleep, Jitter) ->
+  Log = logger:start([john, paul, ringo, george]),
+  A = worker:start(john, Log, 13, Sleep, Jitter),
+  B = worker:start(paul, Log, 23, Sleep, Jitter),
+  C = worker:start(ringo, Log, 36, Sleep, Jitter),
+  D = worker:start(george, Log, 49, Sleep, Jitter),
+  worker:peers(A, [B, C, D]),
+  worker:peers(B, [A, C, D]),
+  worker:peers(C, [A, B, D]),
+  worker:peers(D, [A, B, C]),
+  timer:sleep(5000),
+  logger:stop(Log),
+  worker:stop(A),
+  worker:stop(B),
+  worker:stop(C),
+  worker:stop(D).
